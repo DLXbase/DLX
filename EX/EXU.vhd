37ce04939ecd5cd895aec432f60520bcd9a79a4d
Stefanolovi <stefanoloviselli5@gmail.com>
2024-10-05 16:42:58 +0200

    mistake

--- EX/EXU.vhd
+++ EX/EXU.vhd
@@ -1,12 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.alu_type.all;
 
 entity EXU is
 	generic (N: integer := 32);
 	Port(CLK : in std_logic;
 		RST : in std_logic;
 		CW : in std_logic_vector(3 downto 0);
-		ALU_FUNC : in TYPE_OP;
+		ALU_FUNC : in alu_type;
 		NPC : in std_logic_vector(N-1 downto 0);
 		A_REG : in std_logic_vector(N-1 downto 0);
 		B_REG : in std_logic_vector(N-1 downto 0);
